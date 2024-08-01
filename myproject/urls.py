4d5c6cf2c94c5c165369b4fe3966fc2020079ff5
Madina <mmedikoshh@gmail.com>
2024-08-01 15:33:57 +0500

    Добавлены миграции и тестовые данные

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from products.views import home
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('products.urls')),
+       path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('api/', include('products.urls')),
+
 ]
 
 if settings.DEBUG:
--- products/views.py
+++ products/views.py
@@ -3,9 +3,13 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
+from django.http import HttpResponse
 
 class ProductListView(APIView):
     def get(self, request, *args, **kwargs):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True, context={'request': request})
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+def home(request):
+    return HttpResponse("Welcome to the Product API!")
